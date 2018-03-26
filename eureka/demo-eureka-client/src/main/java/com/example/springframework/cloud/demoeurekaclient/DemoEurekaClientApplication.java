3a3bdb9ff402f1c398a8c7e7ab01248948fa9684
wangzhihao <zhihao.wang@1mifudao.com>
2018-03-26 16:17:03 +0800

    eureka client demo

---
+++ eureka/demo-eureka-client/src/main/java/com/example/springframework/cloud/demoeurekaclient/DemoEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.example.springframework.cloud.demoeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class DemoEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEurekaClientApplication.class, args);
+    }
+}
