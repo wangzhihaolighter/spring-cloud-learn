341e990abb0032afa356116a422d96f15d7a273f
wangzhihao <zhihao.wang@1mifudao.com>
2018-03-27 16:54:59 +0800

    eureka consumer feign:声明式服务调用客户端

--- eureka/eureka-consumer-feign/src/main/java/com/example/springframework/cloud/eurekaconsumerfeign/web/SimpleController.java
+++ eureka/eureka-consumer-feign/src/main/java/com/example/springframework/cloud/eurekaconsumerfeign/web/SimpleController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SimpleController {
@@ -14,7 +13,7 @@ public class SimpleController {
     DemoEurekaClient demoEurekaClient;
 
     @RequestMapping("/")
-    public String home(){
+    public String home() {
         return "eureka consumer feign";
     }
 
