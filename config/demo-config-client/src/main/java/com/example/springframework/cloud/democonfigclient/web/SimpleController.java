214b5f8d612e3de73d03b5bad3c2db5839c5fb92
wangzhihao <zhihao.wang@1mifudao.com>
2018-03-29 09:27:40 +0800

    config:demo-config-client 客户端，从配置中心获取配置

--- config/demo-config-client/src/main/java/com/example/springframework/cloud/democonfigclient/web/SimpleController.java
+++ config/demo-config-client/src/main/java/com/example/springframework/cloud/democonfigclient/web/SimpleController.java
@@ -1,24 +1,29 @@
 package com.example.springframework.cloud.democonfigclient.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class SimpleController {
 
     @Value(value = "${info.profile}")
     private String infoProfile;
 
+    @Value(value = "${info.username}")
+    private String infoUsername;
+
     @RequestMapping("/")
-    public String home(){
+    public String home() {
         return "demo config client";
     }
 
     @GetMapping("/consumer")
-    public String consumer(){
-        return infoProfile;
+    public String consumer() {
+        return infoProfile + ":" + infoUsername;
     }
 
 }
